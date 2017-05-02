d11f0f90e8a04de8a1726069835f0f1f15c3f548
tghil7 <tghil7@aol.com>
2017-05-01 19:21:07 -0500

    Living well first iteration

---
+++ LivingStyle.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Anicet on 10/18/2015.
+ */
+function playImage()
+{
+  if (document.getElementById("image_link").innerText === "View larger image")
+  {
+    $("#thumb_cottage").attr("src", "cottage_large.jpg");
+    document.getElementById("image_link").innerText ="View smaller image";
+  }
+  else if (document.getElementById("image_link").innerText === "View smaller image")
+  {
+    $("#thumb_cottage").attr("src", "cottage_small.jpg");
+    document.getElementById("image_link").innerText ="View larger image";
+  }
+}
+
+
+$("#myButton").addEventListener("click", function()
+  {
+    confirm("Would you like to get out?");
+  }
+);
+
